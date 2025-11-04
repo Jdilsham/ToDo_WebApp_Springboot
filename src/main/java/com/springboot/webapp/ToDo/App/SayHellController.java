751fcb33608022e620ec8e79b0f94f601a1a5fcd
jdilsham <janithadilsham@gmail.com>
2025-11-04 09:55:38 +0530

    add all

---
+++ src/main/java/com/springboot/webapp/ToDo/App/SayHellController.java
@@ -0,0 +1,14 @@
+package com.springboot.webapp.ToDo.App;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHellController {
+    @RequestMapping("say")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
