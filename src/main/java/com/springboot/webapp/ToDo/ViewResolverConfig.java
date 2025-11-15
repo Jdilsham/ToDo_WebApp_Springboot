9843873d2775ec42f7f60d1a8246397f3851acf9
Temp Dev <temp@example.com>
2025-11-15 23:45:49 +0530

    adfdf

---
+++ src/main/java/com/springboot/webapp/ToDo/ViewResolverConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.webapp.ToDo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ViewResolverConfig {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
